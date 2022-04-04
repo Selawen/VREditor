9257182717c18b34a902367fd51ecac0922bc854
Selawen <37039183+Selawen@users.noreply.github.com>
2022-04-04 11:41:20 +0200

    Added undo & redo buttons in VR

--- Assets/Scripts/PanelButton.cs
+++ Assets/Scripts/PanelButton.cs
@@ -1,19 +1,39 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
- 
+using System.Collections.Generic;
+using UnityEngine.UI;
+
 /// <summary>
 /// toggle block choosing panel on and off
 /// </summary>
 public class PanelButton : MonoBehaviour
 {
-    [SerializeField] private GameObject panel;
-     public void TogglePickerPanel()
+    [SerializeField] private List<GameObject> panels = new List<GameObject>();
+    [SerializeField] private string text1, text2;
+
+    void Awake()
     {
-        if (panel == null)
+        GetComponentInChildren<Text>().text = text1;
+    }
+    public void TogglePickerPanel()
+    {
+        foreach (GameObject panel in panels)
         {
-            Debug.Log("no panel set");
-            return;
+            if (panel == null)
+            {
+                Debug.Log("no panel set");
+                return;
+            }
+            panel.SetActive(!panel.activeSelf);
         }
-        panel.SetActive(!panel.activeSelf);
+        
+        Text label = GetComponentInChildren<Text>();
+        if (label.text == text1)
+        {
+            label.text = text2;
+        } else
+        {
+            label.text = text1;
+        }
     }
 }
